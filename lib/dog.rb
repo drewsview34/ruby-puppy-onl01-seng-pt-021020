518ec8ece21a1720e5e477afdc5dae3f51658e45
Andrew Hinojosa <drews_view@hotmail.com>
2020-03-04 07:44:37 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -10,18 +10,20 @@ class Dog
   
   def initialize(name)
     @name = name
-    @@all << self
+    save
   end
   
   def self.all
     @@all
   end
   
-
-  
   def self.print_all
     @@all.each { |puppy| puts "#{puppy.name}"}
   end
+  
+  def save
+    @@all << self
+  end 
   
   def self.clear_all
     @@all.clear
