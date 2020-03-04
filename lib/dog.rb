952437a3a844445bb86ce1e86a9047a49a5e2178
Andrew Hinojosa <drews_view@hotmail.com>
2020-03-04 07:23:34 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,26 @@
+require 'pry'
+
 # Add your code here
+
+class Dog
+  
+  attr_accessor :name 
+  
+  @@all = []
+  
+  def initiliaze(name)
+    @name = name
+    @@all = self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.print_all
+    @@all.each do |song|
+      puts "#{song}"
+    end
+  end
+  
+end
